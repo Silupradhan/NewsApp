01933905a1453214620f29b42b89b3adde2acb1d
Ashutosh pradhan <122736544+Silupradhan@users.noreply.github.com>
2024-05-22 01:25:57 +0530

    creating a news app

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  c="jhon";
   render() {
     return (
       <div>
-        hello my first react class based components {this.c}  {/* we can create and call a variable using THIS in class based componets*/}
+         <Navbar/>
+         <News/>
       </div>
     )
   }
