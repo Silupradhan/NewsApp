7cae62e5f610949838c818cbff97a3c2fa0895d2
Ashutosh pradhan <122736544+Silupradhan@users.noreply.github.com>
2024-05-22 10:24:28 +0530

    fetching api and create newsItem

--- newsapp/src/components/News.js
+++ newsapp/src/components/News.js
@@ -5,10 +5,24 @@ export class News extends Component {
   render() {
     return (
       <div>
-        this is a news app
+        {/* this is a news app
+        <NewsItem title="News Title" description="News Description"/>
         <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
+        <NewsItem/> */}
+        <div className='container my-3'>
+          <h2>DailyNews-Top Headline</h2>
+          <div className='row'>
+            <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description"/>
+            </div>
+            <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description"/>
+            </div>
+            <div className='col-md-4'>
+            <NewsItem title="News Title" description="News Description"/>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
